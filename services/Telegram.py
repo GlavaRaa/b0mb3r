ae80796f220ceec3a62f2703b6fb48229c9a3fc0
GlavaRaa <93398915+GlavaRaa@users.noreply.github.com>
2024-10-05 14:12:15 +0300

    Delete services directory

--- services/Telegram.py
+++
@@ -1,6 +0,0 @@
-from service import Service
-
-
-class Telegram(Service):
-    def send_sms(self):
-        self.session.post('https://my.telegram.org/auth/send_password', data={'phone': '+' + self.formatted_phone})
--- services/Telegramdel2.py
+++
@@ -1,9 +0,0 @@
-from service import Service
-
-
-class Telegramdel2(Service):
-    async def run(self):
-        await self.post(
-            "https://my.telegram.org/auth?to=delete",
-            data={"phone": "+" + self.formatted_phone},
-        )
